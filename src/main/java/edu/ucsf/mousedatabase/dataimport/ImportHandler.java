4f8933ce166aa402ee2d9c558ccdcf9dadbb969f
Jonathan Scoles <scoles@gmail.com>
2012-06-13 19:04:09 -0700

    Update names of data uploads

--- src/main/java/edu/ucsf/mousedatabase/dataimport/ImportHandler.java
+++ src/main/java/edu/ucsf/mousedatabase/dataimport/ImportHandler.java
@@ -86,9 +86,9 @@ public class ImportHandler
     //todo read from user-defined xml file at runtime
     importDefinitions = new ArrayList<ImportDefinition>();
     //make sure the ID is the same as the position in the collection
-    importDefinitions.add(new ImportDefinition(0,"PI to PI Transfer Upload","Create change requests to add holder to mice based on LARC mouse transfer report"));
-    importDefinitions.add(new ImportDefinition(1,"New Purchases Upload","Create submissions for new mice or change requests to add holders to existing mice based on a LARC mouse purchases report"));
-    importDefinitions.add(new ImportDefinition(2,"Transfer from other Instutions Upload","Create submissions for new mice or change requests to add holders to existing mice based on a LARC institutional transfer report"));
+    importDefinitions.add(new ImportDefinition(0,"Transfer Data Upload (TDU)","Create change requests to add holder to mice based on LARC mouse transfer report"));
+    importDefinitions.add(new ImportDefinition(1,"Purchase Data Upload (PDU)","Create submissions for new mice or change requests to add holders to existing mice based on a LARC mouse purchases report"));
+    importDefinitions.add(new ImportDefinition(2,"Import Data Upload (IDU)","Create submissions for new mice or change requests to add holders to existing mice based on a LARC institutional transfer report"));
     
   }
 
