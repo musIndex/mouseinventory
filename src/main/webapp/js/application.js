29fc95e0946e1fa2a08c42e0f97eae8d94919026
Jonathan Scoles <scoles@gmail.com>
2012-09-17 22:00:47 -0700

    Fix checkbox serializing

--- src/main/webapp/js/application.js
+++ src/main/webapp/js/application.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
   function submitParentForm(e,args){
 	  args = args || {};
 	  var form = $(this).closest('form');
-	  window.location.href = form.attr('action') + '?' + form.find('select[name!=pagenum_select],form checkbox').serialize() + "&" + $.param(args);
+	  window.location.href = form.attr('action') + '?' + form.find('select[name!=pagenum_select],input[type=checkbox]').serialize() + "&" + $.param(args);
   }
 });
 
