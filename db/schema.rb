db9fc8308b34043236d14fe1300d144d9c10c334
Jonathan Scoles <scoles@gmail.com>
2013-08-08 22:26:45 -0700

    hide empty labels on home page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130809042102) do
+ActiveRecord::Schema.define(:version => 20130809052145) do
 
   create_table "changerequest", :force => true do |t|
     t.integer "mouse_id"
