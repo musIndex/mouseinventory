95b64fa509649abe103f7865fb27fc72cf059720
ebegalka <ethanbegalka@gmail.com>
2020-04-09 14:16:49 -0400

    Update 20130723052300_add_need_help_settings.rb
    Changed demo email

--- db/migrate/20130723052300_add_need_help_settings.rb
+++ db/migrate/20130723052300_add_need_help_settings.rb
@@ -4,7 +4,7 @@ class AddNeedHelpSettings < ActiveRecord::Migration
 
 INSERT INTO `settings` (`category_id`, `name`, `label`, `setting_value`,`position`)
 VALUES
-(11,'','Request a demonstration','<a href="mailto:admin.mousedatabase@ucsf.edu">admin.mousedatabase@ucsf.edu</a>',0),
+(11,'','Request a demonstration','<a href="mailto:ORA.CARRodentDatabase@msu.edu">ORA.CARRodentDatabase@msu</a>',0),
 (11,'','Ask a Neighbor',"A number of people have volunteered to provide their neighbors with assistance in navigating the database, submitting change requests and entering new submissions.<br><br>To find the name of someone in your neighborhood who can help you with database questions, go to the <a href='/mouseinventory/FacilityReport.jsp'>Facilities List.</a>",1)
 
 EOF
