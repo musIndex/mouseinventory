14c31f0574b7214aa4bccd8639b5e406cff60ac1
willin22 <willin22@msu.edu>
2020-06-03 11:31:50 -0400

    Added filter and changed META-INF\context.xml

--- src/main/java/edu/ucsf/mousedatabase/valves/BasicValve.java
+++ src/main/java/edu/ucsf/mousedatabase/valves/BasicValve.java
@@ -1,6 +1,9 @@
 package edu.ucsf.mousedatabase.valves;
 
 import org.apache.catalina.valves.ValveBase;
+
+import edu.ucsf.mousedatabase.Log;
+
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import java.io.IOException;
@@ -10,8 +13,7 @@ public class BasicValve extends ValveBase {
 	
 	 public void invoke(Request request, Response response) throws IOException, ServletException {
 		 
-		 System.out.println("This should do something when a page is loaded.");
-		 
+		 Log.Info("This should do something when a page is loaded.");
 		 getNext().invoke(request, response);
 	 }
 
