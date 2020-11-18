b8d558bca9de84fa4330a8a0f7fea683b72fd9c4
willin22 <willin22@msu.edu>
2020-11-18 18:07:52 -0500

    Undoing test commit.

--- src/main/java/edu/ucsf/mousedatabase/filters/BasicFilter.java
+++ src/main/java/edu/ucsf/mousedatabase/filters/BasicFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletRequestWrapper;
 
 import edu.ucsf.mousedatabase.Log;
 
@@ -29,35 +29,35 @@ public class BasicFilter implements Filter {
             Log.Info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 
             String host;
-//            try {
-//                host = new URI(httpRequest.getRequestURL().toString()).getHost();
-//
-//                if (host.equals("localhost")) {
-//                    Log.Info("Access attempt on localhost... allowing.");
-//                    chain.doFilter(request, response);
-//                    return;
-//                }
-//                String userName = httpRequest.getHeader("X-MS-CLIENT-PRINCIPAL-NAME");
-//                String userId = httpRequest.getHeader("X-MS-CLIENT-PRINCIPAL-ID");
-//
-//                Log.Info("Access attempt by <" + userName + ">, oid " + userId);
-//                if (userId.isEmpty()) {
-//                    // No user info. Redirect to login.
-//                    httpResponse.sendRedirect("https://" + host + "/.auth/login/aad");
-//                    return;
-//                }
-//
-//                if (adminList.contains(userId)) {
-//                    Log.Info("User is an admin. Allow access.");
-//
-//                    chain.doFilter(request, response);
-//                    return;
-//                }
-//                httpRequest.getRequestDispatcher("/accessDenied.jsp").forward(httpRequest, httpResponse);
-//                return;
-//            } catch (URISyntaxException error) {
-//                error.printStackTrace();
-//            }
+            try {
+                host = new URI(httpRequest.getRequestURL().toString()).getHost();
+
+                if (host.equals("localhost")) {
+                    Log.Info("Access attempt on localhost... allowing.");
+                    chain.doFilter(request, response);
+                    return;
+                }
+                String userName = httpRequest.getHeader("X-MS-CLIENT-PRINCIPAL-NAME");
+                String userId = httpRequest.getHeader("X-MS-CLIENT-PRINCIPAL-ID");
+
+                Log.Info("Access attempt by <" + userName + ">, oid " + userId);
+                if (userId.isEmpty()) {
+                    // No user info. Redirect to login.
+                    httpResponse.sendRedirect("https://" + host + "/.auth/login/aad");
+                    return;
+                }
+
+                if (adminList.contains(userId)) {
+                    Log.Info("User is an admin. Allow access.");
+
+                    chain.doFilter(request, response);
+                    return;
+                }
+                httpRequest.getRequestDispatcher("/accessDenied.jsp").forward(httpRequest, httpResponse);
+                return;
+            } catch (URISyntaxException error) {
+                error.printStackTrace();
+            }
         }
     }
    
