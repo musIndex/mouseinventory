72c04fed368f53b19ef4f6bbb26e2717701e31a5
cardime2 <cardime2@msu.edu>
2021-08-07 13:58:40 -0400

    updated email icon

--- src/main/java/edu/ucsf/mousedatabase/HTMLGeneration.java
+++ src/main/java/edu/ucsf/mousedatabase/HTMLGeneration.java
@@ -3718,7 +3718,7 @@ public class HTMLGeneration {
     public static String getWebsiteFooter() {
 
         //Database version
-        String version = "4.2.02";
+        String version = "4.2.03";
         //Current date
         String year = "2021";
         //Email of database administrator
