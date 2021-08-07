dad1220294da0cbec566c32d0682fadcaf254dd9
cardime2 <cardime2@msu.edu>
2021-08-07 13:44:06 -0400

    updated eyeglass icon

--- src/main/java/edu/ucsf/mousedatabase/HTMLGeneration.java
+++ src/main/java/edu/ucsf/mousedatabase/HTMLGeneration.java
@@ -3718,7 +3718,7 @@ public class HTMLGeneration {
     public static String getWebsiteFooter() {
 
         //Database version
-        String version = "4.2.01";
+        String version = "4.2.02";
         //Current date
         String year = "2021";
         //Email of database administrator
