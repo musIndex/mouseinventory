0077cca268a1b268eb736ebcb7216f446585a633
estelle <estellewall@gmail.com>
2022-01-02 16:53:11 -0800

    Fix new Jax site JSON connect to retrieve MGI ID

--- src/main/java/edu/ucsf/mousedatabase/JaxMiceConnect.java
+++ src/main/java/edu/ucsf/mousedatabase/JaxMiceConnect.java
@@ -4,13 +4,19 @@ package edu.ucsf.mousedatabase;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.jsoup.Connection;
 //Added Jsoup imports -EW
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.google.gson.Gson;
 
 
 
@@ -19,11 +25,13 @@ public class JaxMiceConnect {
   //URL changed for jax, example https://www.jax.org/strain/007676 -EW
   //private static final String baseUrl = "http://jaxmice.jax.org/strain/"; -EW turned off
   private static final String baseUrl = "https://www.jax.org/strain/";
+  private static final String baseJson = "https://3ddatasheet-services-prod.azurewebsites.net/Data/GetComponentData?stockNumber=";
+  private static final String tailJson = "&componentType=HowItsMade";
   //private static final String urlTail = ".html"; -EW not used in new stock URL
   //alleleRegex won't work, Alele Symbol: <a id="alleleid_54647_symbol" href="http://www.informatics.jax.org/accession/MGI:3716464"
   //private static final String alleleRegex = "Allele&nbsp;Symbol[^\\n]*\\n[^\\n]*MGI:([\\d]+)";
-  private static final String alleleRegex ="[href*=/accession/MGI:(\\d{7})]";
-  
+  private static final String alleleRegex ="[href*=/allele/MGI:(\\d{7})]";
+  //private static final String alleleRegex ="[href*=/accession/MGI:(\\d{7})]";
   private static final String stockNumberRegex = "([\\d]{4,6})";
   
 
@@ -49,30 +57,55 @@ public class JaxMiceConnect {
       return mgiNumbers;
     }
     */
-    String urlConnect = baseUrl + extractedNumber;
-    Document document = Jsoup.connect(urlConnect).timeout(30000).get();
+    //String urlConnect = baseUrl + extractedNumber;
     
-    Elements links = document.select("[href*=/accession/MGI:]");
+    String urlConnect = "https://www.jax.org/strain/" + extractedNumber;
+    Log.Info(urlConnect);
+    String jsonConnect = baseJson + extractedNumber + tailJson;
     
-        Pattern expression = Pattern.compile("MGI:(\\d{7})");//will also find gene ID if 7 digits long
-        Matcher matcher = expression.matcher(links.toString());
-        //If loop will find 1st occurance and ignore gene MGI ID number
-        if (matcher.find()){
-        //This Prints out MGI:and number use 1 to print out the 1st capturing group
-        System.out.println(matcher.group(1));
-        String possibleMgiNumber =matcher.group(1);
+    //Document document = Jsoup.connect(urlConnect).timeout(30000).get();        
+    String data = Jsoup.connect(jsonConnect).ignoreContentType(true).execute().body();  
+    Log.Info("Json Connect: " + data);
+    
+    //Elements links = document.select("a");
+     //Log.Info("Links test = " + links);
+     
+        //Pattern expression = Pattern.compile("MGI:(\\d{7})");//will also find gene ID if 7 digits long
+        //Matcher matcher = expression.matcher(links.toString());
         
-        try
+        //If loop will find 1st occurance and ignore gene MGI ID number
+    
+    Gson gson = new Gson();
+    Map<String, Object> asMap = gson.fromJson(data, Map.class);
+    List<Map<String, Object>> geneticInfos = (List) asMap.get("geneticInfo");
+    for (Map<String, Object> geneticInfo : geneticInfos) {
+        // check that the response contains mgiAccessionID
+        if (geneticInfo.containsKey("mgiAccessionID")) 
         {
-          int mgiNumber = Integer.parseInt(possibleMgiNumber);
-          Log.Info("Conversion from jax: " + extractedNumber + " to MGI: " + mgiNumber);
-          mgiNumbers.add(mgiNumber);
+            String mgiAccessionID = (String) geneticInfo.get("mgiAccessionID");
+            if (mgiAccessionID != null) {
+                Pattern expression = Pattern.compile("MGI:(\\d{7})");
+                Matcher matcher = expression.matcher(mgiAccessionID);
+                if (matcher.find()){
+                    //This Prints out MGI:and number use 1 to print out the 1st capturing group
+                    System.out.println(matcher.group(1));
+                    String possibleMgiNumber =matcher.group(1);
+                    
+                
+                    try
+                    {
+                      int mgiNumber = Integer.parseInt(possibleMgiNumber);
+                      Log.Info("Conversion from jax: " + extractedNumber + " to MGI: " + mgiNumber);
+                      mgiNumbers.add(mgiNumber);
+                    }
+                    catch(NumberFormatException e)
+                    {
+                      Log.Error("Failed to convert " + possibleMgiNumber + " to an MGI number",e);
+                    }
+                    }
+            }
         }
-        catch(NumberFormatException e)
-        {
-          Log.Error("Failed to convert " + possibleMgiNumber + " to an MGI number",e);
-        }
-        }
+    }
 
     return mgiNumbers;
 
